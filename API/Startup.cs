d7fcaaa7ffaf263a20b128d39c3fd7a89510a194
Michal Makowski <michmakow@gmail.com>
2022-10-13 20:54:35 +0200

    CRUD for activities, AutoMapper, MediatR pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,6 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Persistence;
@@ -35,6 +38,9 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
